bf3d40295624f27e296651aea508bca77426f0ff
GagoChad <gagika57@gmail.com>
2023-12-21 23:37:30 +0500

    feat: add full navbar, toggle effects

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -1,14 +1,37 @@
+import { motion } from 'framer-motion'
 import { useState } from 'react'
 
-import avatar from './public/avatar.png'
+import { useMediaQuery } from '../../util/useMediaQuery'
+
+const navMotion = {
+	visible: {
+		opacity: 1,
+
+		transition: {
+			when: 'beforeChildren',
+			staggerChildren: 0.15,
+		},
+	},
+	hidden: {
+		opacity: 0,
+	},
+}
+
+const itemMotion = {
+	visible: { opacity: 1, x: 0 },
+	hidden: { opacity: 0, x: -100 },
+}
+
+import avatar from '/public/avatar.png'
 
 export default function Nav() {
-	const [toggle, setToggle] = useState(false)
+	const [toggled, setToggled] = useState(false)
+	const matches = useMediaQuery('(min-width: 1280px)')
 
 	return (
-		<nav className="relative mx-8 mb-24 flex justify-between items-center pt-12 pb-6 font-medium md:mx-16 lg:mx-32">
+		<nav className='relative mx-8 mb-24 flex justify-between items-center pt-12 pb-6 font-medium md:mx-16 lg:mx-32'>
 			<svg
-				className="absolute bottom-0 left-1/2 -translate-x-1/2"
+				className='absolute bottom-0 left-1/2 -translate-x-1/2'
 				fill='none'
 				xmlns='http://www.w3.org/2000/svg'
 				width='250'
@@ -28,15 +51,59 @@ export default function Nav() {
 
 			{/* Title */}
 
-			<h1 className="text-lg font-bold">
+			<h1 className='text-lg font-bold'>
 				<a href='/'>Hua.</a>
 			</h1>
 
-			<div className="space-y-1">
-				<span className="block h-0.5 w-8 bg-black"></span>
-				<span className="block h-0.5 w-6 bg-black"></span>
-				<span className="block h-0.5 w-4 bg-black"></span>
-			</div>
+			{matches && (
+				<div className=' flex gap-12'>
+					<a href='/'>Home</a>
+					<a href='/services'>Services</a>
+					<a href='/contact'>Contact</a>
+				</div>
+			)}
+
+			{!matches && (
+				<div
+					onClick={() => setToggled((prevToggle) => !prevToggle)}
+					className='space-y-1.5 cursor-pointer z-50'
+				>
+					<motion.span
+						animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0 }}
+						className='block h-0.5 w-8 bg-black'
+					></motion.span>
+					<motion.span
+						animate={{ width: toggled ? 0 : 24 }}
+						className='block h-0.5 w-6 bg-black'
+					></motion.span>
+					<motion.span
+						animate={{
+							rotateZ: toggled ? -45 : 0,
+							y: toggled ? -8 : 0,
+							width: toggled ? 32 : 16,
+						}}
+						className='block h-0.5 w-4 bg-black'
+					></motion.span>
+				</div>
+			)}
+			{toggled && !matches && (
+				<motion.div
+					animate={{ opacity: 1, x: 0 }}
+					initial={{ opacity: 1, x: 25 }}
+					className='fixed flex bg-white bottom-0 left-0 w-full h-full items-center justify-center'
+				>
+					<motion.div
+						variants={navMotion}
+						animate="visible"
+						initial="hidden"
+						className='flex flex-col gap-24 text-lg'
+					>
+						<motion.a variants={itemMotion} href='/'>Home</motion.a>
+						<motion.a variants={itemMotion} href='/services'>Services</motion.a>
+						<motion.a variants={itemMotion} href='/contact'>Contact</motion.a>
+					</motion.div>
+				</motion.div>
+			)}
 		</nav>
 	)
 }
